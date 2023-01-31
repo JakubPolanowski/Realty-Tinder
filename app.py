e42937548e960165430d63f1f02975ea81179dc9
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-31 03:11:02 -0500

    implemented page picker

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session
 from glob import glob
 from pathlib import Path
@@ -30,68 +30,82 @@ def error404(e):
     return render_template('404.html'), 404
 
 
-@app.route('/listings/<string:listings_file>', defaults={'index': 0})
-@app.route('/listings/<string:listings_file>/<int:index>')
+@app.route('/listings/<string:listings_file>', defaults={'index': 0}, methods=["GET", "POST"])
+@app.route('/listings/<string:listings_file>/<int:index>', methods=["GET", "POST"])
 def listings(listings_file: str, index: int):
 
-    if not (listing_path := Path(f"data/listings/{listings_file}.xlsx")).exists():
-        return render_template('404.html', subtitle='The listings file does not exist'), 404
+    if request.method == "POST":
+        form = request.form
 
-    if session.get('listings_file') != listings_file:
-        df = pd.read_excel(listing_path)
-        df['list date'] = df['list date'].dt.strftime('%m/%d/%Y')
+        if 'page' in form:
+            return redirect(
+                url_for('listings', listings_file=listings_file,
+                        index=int(form['page'])-1)
+            )
 
-        session['listings_file'] = listings_file
-        session['listings'] = df.to_json(orient='records')
+        else:  # TODO pick proper error code
+            return render_template('404.html', subtitle="Invalid form POST"), 404
 
-    dataset = json.loads(session['listings'])
-    if index >= len(dataset):
-        return render_template('404.html', subtitle=f'Out of bounds! There are only {len(dataset)} listing(s), {index+1} is invalid.'), 404
-
-    listing = dataset[index]
-
-    # special values
-    try:
-        listing['pretty price'] = f"{listing['price']:,}"
-    except TypeError:
-        listing['pretty price'] = listing['price']
-
-    try:
-        listing['pretty built'] = f"{listing['year built']:.0f}"
-    except TypeError:
-        listing['pretty built'] = listing['year built']
-
-    try:
-        listing['pretty hoa'] = f"{listing['hoa fee']:,.0f}"
-    except TypeError:
-        listing['pretty hoa'] = listing['hoa fee']
-
-    try:
-        listing['pretty sqft'] = f"{listing['interior sqft']:,.0f}"
-    except TypeError:
-        listing['pretty sqft'] = listing['interior sqft']
-
-    try:
-        listing['price per sqft'] = f"{listing['price']/listing['interior sqft']:,.2f}"
-    except TypeError:
-        listing['price per sqft'] = ""
-
-    try:
-        listing['pretty acres'] = f"{listing['lot sqft']/43560:,.2f}"
-    except TypeError:
-        listing['pretty acres'] = ""
-
-    try:
-        listing['pretty minutes'] = f"{listing['Minutes to Work']:,.0f}"
-    except:
-        listing['pretty minutes'] = listing['Minutes to Work']
-
-    if listing['Minutes to Work'] <= 15:
-        listing['minutes class'] = "has-text-success"
-    elif listing['Minutes to Work'] <= 25:
-        listing['minutes class'] = 'has-text-warning'
     else:
-        listing['minutes class'] = 'has-text-danger'
 
-    # render
-    return render_template('listings.html', listings_file=listings_file, index=index, total=len(dataset), listing=listing)
+        if not (listing_path := Path(f"data/listings/{listings_file}.xlsx")).exists():
+            return render_template('404.html', subtitle='The listings file does not exist'), 404
+
+        if session.get('listings_file') != listings_file:
+            df = pd.read_excel(listing_path)
+            df['list date'] = df['list date'].dt.strftime('%m/%d/%Y')
+
+            session['listings_file'] = listings_file
+            session['listings'] = df.to_json(orient='records')
+
+        dataset = json.loads(session['listings'])
+        if index >= len(dataset):
+            return render_template('404.html', subtitle=f'Out of bounds! There are only {len(dataset)} listing(s), {index+1} is invalid.'), 404
+
+        listing = dataset[index]
+
+        # special values
+        try:
+            listing['pretty price'] = f"{listing['price']:,}"
+        except TypeError:
+            listing['pretty price'] = listing['price']
+
+        try:
+            listing['pretty built'] = f"{listing['year built']:.0f}"
+        except TypeError:
+            listing['pretty built'] = listing['year built']
+
+        try:
+            listing['pretty hoa'] = f"{listing['hoa fee']:,.0f}"
+        except TypeError:
+            listing['pretty hoa'] = listing['hoa fee']
+
+        try:
+            listing['pretty sqft'] = f"{listing['interior sqft']:,.0f}"
+        except TypeError:
+            listing['pretty sqft'] = listing['interior sqft']
+
+        try:
+            listing['price per sqft'] = f"{listing['price']/listing['interior sqft']:,.2f}"
+        except TypeError:
+            listing['price per sqft'] = ""
+
+        try:
+            listing['pretty acres'] = f"{listing['lot sqft']/43560:,.2f}"
+        except TypeError:
+            listing['pretty acres'] = ""
+
+        try:
+            listing['pretty minutes'] = f"{listing['Minutes to Work']:,.0f}"
+        except:
+            listing['pretty minutes'] = listing['Minutes to Work']
+
+        if listing['Minutes to Work'] <= 15:
+            listing['minutes class'] = "has-text-success"
+        elif listing['Minutes to Work'] <= 25:
+            listing['minutes class'] = 'has-text-warning'
+        else:
+            listing['minutes class'] = 'has-text-danger'
+
+        # render
+        return render_template('listings.html', listings_file=listings_file, index=index, total=len(dataset), listing=listing)
