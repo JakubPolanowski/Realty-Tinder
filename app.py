522dd153a87a67a2db2b9bfc2943b48008fb61f1
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-30 23:16:59 -0500

    added loading of listings spreadsheet

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_session import Session
 from glob import glob
 from pathlib import Path
+import uuid
+import pandas as pd
 
 app = Flask(__name__)
+
+# secret key is used only for client side sessions
+# therefore randomly generating at runtime seems preferrable
+app.secret_key = uuid.uuid4().hex
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route('/')
@@ -15,6 +26,8 @@ def index():
 
 @app.route('/listings/<string:listings_file>')
 def listings(listings_file: str):
-    # TODO read listing file then parse
+
+    session['listings'] = pd.read_excel(
+        f"data/listings/{listings_file}.xlsx").to_json(orient='records')
 
     return render_template('listings.html', listings_file=listings_file)
