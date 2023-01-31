18d7901e7f639e0f56f57e88253a9e14b2ad44b8
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-30 22:12:36 -0500

    implemented listing of listing files

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from glob import glob
+from pathlib import Path
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    files = glob("data/listings/*.xlsx")
+    files = [(p.stem, p) for p in [Path(f) for f in files]]
+    return render_template('index.html', files=files)
